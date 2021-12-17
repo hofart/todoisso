bd7341406a75c5be12e720185168a118757b4d2b
hofart <hofart.matheus@gmail.com>
2021-12-17 16:41:14 -0300

    subindo alteracoes

--- src/js/view.js
+++ src/js/view.js
@@ -2,7 +2,7 @@ export default class View {
   constructor() {}
 
   #todolist = []
-  #id = 1
+  #id = Math.floor(Math.random() * 9999)
 
   #items = document.getElementById('items')
   #li = document.querySelectorAll('task_list_item')
@@ -13,93 +13,72 @@ export default class View {
     this.#btnAdd.addEventListener('click', () => {
       const { title, content } = this.#form
 
-      // if any input field is empty, cant submit
       if(!title.value.trim() || !content.value.trim()) return false
 
-      //increment id and push value to array
       this.#id++
 
       this.#todolist.push({
         title: title.value,
         content: content.value,
-        id: this.#id
+        id: this.#id,
+        done: 'false'
       })
 
-      //clear input after submit
       this.#form.reset()
-      //show div only if it has content
-      this.#todolist ? this.#items.style.display = 'block' : this.#items.style.display = 'none'
-      //calling methods
-      this.createList(this.#todolist, this.#items)
-      this.deleteTask(this.#todolist)
-      this.checkTask(this.#todolist)
+
+      if(this.#todolist) {
+        this.#items.style.display = 'block'
+
+        this.createList(this.#items)
+        this.deleteTask()
+        this.doneOrUndone()
+      } else {
+        this.#items.style.display = 'none'
+      }
     })  
   }
 
-  createList(list, ul) {
-    if(list) {
-      const li = this.#todolist.map(item => `
-        <li class="task_list_item" id="task-${item.id}">
-          <div class="task_list_item__checkbox">
-            <input type="checkbox" class="task_list_item__input">
-            <div class="task_list_item__checkbox--body">
-              <p class="task_list_item__input--title">${item.title}</p>
-              <p class="task_list_item__input--description">${item.content}</p>
-            </div>
+  createList(ul) {
+    const li = this.#todolist.map(item => `
+      <li class="task_list_item" id="task-${item.id}">
+        <div class="task_list_item__checkbox">
+          <input type="checkbox" class="task_list_item__input" data-id="${item.id}">
+          <div class="task_list_item__checkbox--body">
+            <p class="task_list_item__input--title">${item.title}</p>
+            <p class="task_list_item__input--description">${item.content}</p>
           </div>
-          <div class="task_list_item__delete">
-            <a href="#!" class="delete_task" data-id="${item.id}">
-              <i class="far fa-trash-alt"></i>
-            </a>
-          </div>
-        </li>
-      `).join('')
+        </div>
+        <div class="task_list_item__delete">
+          <a href="#!" class="delete_task" data-id="${item.id}">
+            <i class="far fa-trash-alt"></i>
+          </a>
+        </div>
+      </li>
+    `).join('')
 
-      ul.innerHTML = li
-    }
+    ul.innerHTML = li
   }
 
-  checkTask(list) {
-    if(list) {
-      const nodes = this.#items.querySelectorAll('.task_list_item__input')
+  doneOrUndone() {
+    const nodes = this.#items.querySelectorAll('.task_list_item__input')
 
-      nodes.forEach(e => e.addEventListener('click', () => {
-        let styles = `
-          text-decoration: line-through;
-          font-style: italic;
-        `
-        let noStyles = `
-          text-decoration: none;
-          font-style: none;
-        `
-
-        // if input:checked set some styles
-        if(e.checked) {
-          e.parentNode.lastElementChild.firstElementChild.style = styles
-          e.parentNode.lastElementChild.lastElementChild.style = styles
-        } else {
-          e.parentNode.lastElementChild.firstElementChild.style = noStyles
-          e.parentNode.lastElementChild.lastElementChild.style = noStyles
-        }
-      }))
-    }
-  }
-
-  deleteTask(list) {
-    if(list) {
-      const nodes = this.#items.querySelectorAll('.delete_task')  
-
-      // add envent click on all delete button
-      nodes.forEach(e => e.addEventListener('click', () => {
-        // back to father element and get data-attr
-        const li = e.parentNode
+    nodes.forEach(e => e.addEventListener('click', () => {
         const data = e.getAttribute('data-id')
+        this.#todolist.findIndex(el => {
+          el.id == data && e.checked ? el.done = 'true' : el.done = 'false'
+        })
+    }))
+  }
 
-        //find element in array and remove, remove li also
-        this.#todolist.splice(this.#todolist.findIndex(el => el.id == data), 1)
-        li.parentNode.remove()
-      }))
-    }
+  deleteTask() {
+    const nodes = this.#items.querySelectorAll('.delete_task')  
+
+    nodes.forEach(e => e.addEventListener('click', () => {
+      const li = e.parentNode
+      const data = e.getAttribute('data-id')
+      this.#todolist.splice(this.#todolist.findIndex(el => el.id == data), 1)
+      li.parentNode.remove()
+    }))
   }
   
   _init() {
